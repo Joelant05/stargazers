a8c044e64c4580cdfa3a0c35148cb5b9051e3164
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2022-07-25 15:07:16 +0100

    upd: move starfield to class
    not yet tested in game

--- BP/scripts/Gameplay/Telescope/Main.ts
+++ BP/scripts/Gameplay/Telescope/Main.ts
@@ -1,8 +1,7 @@
-import { world, Location, Player, MinecraftItemTypes, EntityRaycastOptions, MinecraftEffectTypes, DataDrivenEntityTriggerEvent } from "mojang-minecraft"
+import { world, Player, DataDrivenEntityTriggerEvent } from "mojang-minecraft"
 import { getTime } from "scripts/Utils/time.js"
 import { alert } from "scripts/Utils/alert.js"
-import { giveMainhand } from "scripts/Utils/giveMainhand.js"
-import { spawnInRange } from "scripts/Utils/spawnInRange.js"
+import { Starfield } from "scripts/Gameplay/Telescope/Starfield.js"
 
 export class TelescopeHandler {
     /**
@@ -28,66 +27,13 @@ export class TelescopeHandler {
      * Method called when a telescope is interacted with
      */
     onInteract(event: DataDrivenEntityTriggerEvent) {
-        const ow = world.getDimension('overworld')
         this.player = event.entity as Player
 
         if (!this.isAvailable(event.id)) return
 
-        if (!giveMainhand(MinecraftItemTypes.spyglass, this.player)) return
-        this.inStarfield = true
-        const center = new Location(this.player.location.x, 500, this.player.location.z)
-        this.player.teleport(center, ow, 0, 0)
-        const controller = ow.spawnEntity('star:starfield_controller', center)
-        controller.runCommandAsync('ride @p start_riding @s teleport_rider').then((res) => {
-            if (res.successCount > 0) {
-                ow.runCommand('time set midnight')
-                alert([
-                    { text: "You are now §dStargazing§r " },
-                    { selector: '@s' },
-                    { text: "! Using the §aSpyglass§r, find the §6Magical Star§r and zoom in on it..." }
-                ], this.player)
-                let progress = 0
-                spawnInRange('star:star', center, 14, 14, 8)
-                this.player.addEffect(
-                    MinecraftEffectTypes.resistance,
-                    4000,
-                    25,
-                    false
-                )
-                const tick = world.events.tick.subscribe((eventData) => {
-                    if (eventData.currentTick % 10 === 0) {
-                        // Every 0.5 seconds
-                        if (progress < 12) {
-                            const opts = new EntityRaycastOptions()
-                            opts.maxDistance = 50
-                            const entities = this.player.getEntitiesFromViewVector(opts)
-                            if (entities.length > 0 && entities[0].id === 'star:star' && player.hasComponent('is_baby')) {
-                                if (progress === 0) this.player.onScreenDisplay.setTitle('showstardisplay')
-                                progress++
-                            } else {
-                                this.player.onScreenDisplay.setTitle('hidestardisplay')
-                                progress = 0
-                            }
-                        } else {
-                            this.player.onScreenDisplay.setTitle('hidestardisplay')
-                            this.player.runCommand('clear @s spyglass 0 1')
-                            this.player.runCommand('event entity @e[r=5,type=star:starfield_controller] star:on_complete')
-                            this.player.triggerEvent('star:queue_starfall')
-                            this.inStarfield = false
-
-                            world.events.tick.unsubscribe(tick)
-                        }
-
-                    } else if (eventData.currentTick % 200 === 0) {
-                        // Every 10 seconds
-                        ow.runCommand('time set midnight')
-                    }
-
-                })
-            } else {
-                console.warn('Error running /ride command!')
-            }
-        })
+        const starfield = new Starfield(this.player)
+        starfield.events.onStart = () => this.inStarfield = true
+        starfield.events.onComplete = () => this.inStarfield = false
     }
 
     /**
--- BP/scripts/Gameplay/Telescope/Starfield.ts
+++ BP/scripts/Gameplay/Telescope/Starfield.ts
@@ -1,5 +1,84 @@
-import { EntityRaycastOptions, MinecraftItemTypes, Player, world } from "mojang-minecraft"
+import { world, Location, Player, MinecraftItemTypes, EntityRaycastOptions, MinecraftEffectTypes, Entity } from "mojang-minecraft"
+import { alert } from "scripts/Utils/alert.js"
+import { giveMainhand } from "scripts/Utils/giveMainhand.js"
+import { spawnInRange } from "scripts/Utils/spawnInRange.js"
 
-export function startStarfield(player: Player) {
+export class Starfield {
+    public events: {
+        onStart: () => void
+        onComplete: () => void
+    }
 
+    protected position: Location
+
+    constructor(protected player: Player) {
+        const controller = this.setup()
+        if (!controller) return
+        this.start(controller)
+    }
+
+    setup() {
+        if (!giveMainhand(MinecraftItemTypes.spyglass, this.player)) return
+
+        const ow = world.getDimension('overworld')
+
+        this.position = new Location(this.player.location.x, 500, this.player.location.z)
+        this.player.teleport(this.position, ow, 0, 0)
+        return ow.spawnEntity('star:starfield_controller', this.position)
+    }
+
+    start(controller: Entity) {
+        controller.runCommandAsync('ride @p start_riding @s teleport_rider').then((res) => {
+            if (res.successCount > 0) {
+                const ow = world.getDimension('overworld')
+                ow.runCommand('time set midnight')
+                alert([
+                    { text: "You are now §dStargazing§r " },
+                    { selector: '@s' },
+                    { text: "! Using the §aSpyglass§r, find the §6Magical Star§r and zoom in on it..." }
+                ], this.player)
+                let progress = 0
+                spawnInRange('star:star', this.position, 14, 14, 8)
+                this.player.addEffect(
+                    MinecraftEffectTypes.resistance,
+                    4000,
+                    25,
+                    false
+                )
+                const tick = world.events.tick.subscribe((eventData) => {
+                    if (eventData.currentTick % 10 === 0) {
+                        // Every 0.5 seconds
+                        if (progress < 12) {
+                            const opts = new EntityRaycastOptions()
+                            opts.maxDistance = 50
+                            const entities = this.player.getEntitiesFromViewVector(opts)
+                            if (entities.length > 0 && entities[0].id === 'star:star' && this.player.hasComponent('is_baby')) {
+                                if (progress === 0) this.player.onScreenDisplay.setTitle('showstardisplay')
+                                progress++
+                            } else {
+                                this.player.onScreenDisplay.setTitle('hidestardisplay')
+                                progress = 0
+                            }
+                        } else {
+                            this.events.onComplete()
+
+                            this.player.onScreenDisplay.setTitle('hidestardisplay')
+                            this.player.runCommand('clear @s spyglass 0 1')
+                            this.player.runCommand('event entity @e[r=5,type=star:starfield_controller] star:on_complete')
+                            this.player.triggerEvent('star:queue_starfall')
+
+                            world.events.tick.unsubscribe(tick)
+                        }
+
+                    } else if (eventData.currentTick % 200 === 0) {
+                        // Every 10 seconds
+                        ow.runCommand('time set midnight')
+                    }
+
+                })
+            } else {
+                console.warn('Error running /ride command!')
+            }
+        })
+    }
 }
